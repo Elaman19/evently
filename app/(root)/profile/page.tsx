17fc6829e771fff38eaa0591a5153a0a6a984f47
Elaman19 <30525069+Elaman19@users.noreply.github.com>
2024-02-17 13:19:20 +0600

    added searching

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,15 +1,24 @@
 import { getEventsByUser } from '@/components/lib/actions/event.actions'
+import { getOrdersByUser } from '@/components/lib/actions/order.actions'
+import { IOrder } from '@/components/lib/database/models/order.model'
 import Collection from '@/components/shared/Collection'
 import { Button } from '@/components/ui/button'
+import { SearchParamProps } from '@/types'
 import { auth } from '@clerk/nextjs'
 import Link from 'next/link'
 import React from 'react'
 
-const ProfilePage = async () => {
+const ProfilePage = async ({searchParams}: SearchParamProps) => {
   const {sessionClaims} = auth()
   const userId = sessionClaims?.userId as string
 
-  const organizedEvents = await getEventsByUser({userId, page: 1})
+  const ordersPage = Number(searchParams?.ordersPage) || 1
+  const eventsPage = Number(searchParams?.eventsPage) || 1
+
+  const orders = await getOrdersByUser({userId, page: ordersPage})
+  const orderedEvents = orders?.data.map((order: IOrder) => order.event) || []
+  const organizedEvents = await getEventsByUser({userId, page: eventsPage})
+
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
@@ -23,18 +32,18 @@ const ProfilePage = async () => {
         </div>
       </section>
       
-      {/* <section className='wrapper my-8'>
+      <section className='wrapper my-8'>
         <Collection
-            data={events?.data}
+            data={orderedEvents}
             emptyTitle='No event ticket purchased yet'
             emptyStateSubtext='No worries - plenty of exciting events to explore'
             collectionType='My_Tickets'
             limit={3}
-            page={1}
+            page={ordersPage}
             urlParamName='ordersPage'
-            totalPages={2}
+            totalPages={orders?.totalPages}
         />
-      </section> */}
+      </section>
 
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
@@ -54,9 +63,9 @@ const ProfilePage = async () => {
           emptyStateSubtext='Go create some now'
           collectionType='Events_Organized'
           limit={6}
-          page={1}
+          page={eventsPage}
           urlParamName='eventsPage'
-          totalPages={2}
+          totalPages={organizedEvents?.totalPages}
         />
       </section>
     </>
